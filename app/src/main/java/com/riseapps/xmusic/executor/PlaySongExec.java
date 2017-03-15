eb9c079e297da0ce156d68c83bef995b5374cf42
Naimish Srivastava <naimish.victory@gmail.com>
2017-03-15 18:28:23 +0530

    7th commit

---
+++ app/src/main/java/com/riseapps/xmusic/executor/PlaySongExec.java
@@ -0,0 +1,29 @@
+package com.riseapps.xmusic.executor;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.riseapps.xmusic.model.MusicService;
+import com.riseapps.xmusic.view.MainActivity;
+
+/**
+ * Created by naimish on 14/3/17.
+ */
+
+public class PlaySongExec {
+
+    private Context ctx;
+    private Activity mActivity;
+    private int mPos;
+
+    public PlaySongExec(Context context, int position) {
+        this.ctx = context;
+        this.mPos = position;
+    }
+
+    public void startPlaying() {
+        MusicService musicService =((MainActivity)ctx).getMusicService();
+        musicService.setSong(mPos);
+        musicService.togglePlay();
+    }
+}
